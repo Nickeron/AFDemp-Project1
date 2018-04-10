149d9991ba08f5bffa2c2b68b1cd66b745ec5504
Nick Karamousadakis <digakrit@hotmail.com>
2018-04-10 15:40:42 +0300

    adds automatic migrations and removes manual ones

--- Project1Afdemp/Migrations/Configuration.cs
+++ Project1Afdemp/Migrations/Configuration.cs
@@ -1,16 +1,12 @@
 namespace Project1Afdemp.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Project1Afdemp.DatabaseStuff>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
-            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Project1Afdemp.DatabaseStuff context)
